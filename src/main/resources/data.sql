fc42ea489f4696d8d93b76a22b251dccbdb177e3
VedantWankhade <vedantwankhade17@gmail.com>
2022-06-08 13:12:04 +0530

    Initialize the project and setup database and model class

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into user (userid,upassword,uname) values ('a','a','A');
+insert into user (userid,upassword,uname) values ('b','b','B');
+insert into user (userid,upassword,uname) values ('c','c','C');
+
